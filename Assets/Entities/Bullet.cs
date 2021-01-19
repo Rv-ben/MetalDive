739733d23a2bf9ede26e33ff5048654f046345a6
Canyon-Artuz <canyonartuz@gmail.com>
2021-01-19 11:12:03 -0800

    Gun Attached to Player Model, Shoots and has Effect.
    Gun does not shoot straight
    Effects too large

---
+++ Assets/Entities/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Instantiate the Hit Effect at the given position of the bullet without rotation.
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        // Destroy the effect after 5 seconds.
+        Destroy(effect, 5f);
+        // Destroy the bullet.
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Entities/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    // Where the bullet comes out.
+    public Transform firePoint;
+    // What bullet gets fired.
+    public GameObject bulletPrefab;
+    // The force of the bullet.
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Should be mapped to Left Click for now.
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        // Instantiate a Bullet prefab.
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        // Get the rigidbody of the bullet.
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        // Add instand force in the direction of the firePoint, using bulletForce as a base.
+        rb.AddForce(firePoint.up * bulletForce, ForceMode.Impulse);
+    }
+}
