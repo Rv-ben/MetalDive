f4a3a4a02c49bbb2ff7469d68635cf628e759b00
aimilbc <touyamaaimi927@gmail.com>
2021-01-22 14:17:48 -0800

    enemy movement updated

--- Assets/Entities/EnemyMovement.cs
+++ Assets/Entities/EnemyMovement.cs
@@ -3,39 +3,46 @@ using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
 
+///<summary>
+///Enemy behavior
+/// </summary>
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] float randArea = 10f;
 
-    // Target next position
-    public Transform targetPoint;
-
-    private NavMeshAgent agent;
-
-    // Limit of random distance
-    [SerializeField] float randArea = 5f;
-
-    // Limit of waiting time
     [SerializeField] float waitingTime = 2f;
 
-    // Counting time
     [SerializeField] float countWaitingTime = 0f;
 
-    // Idling distance
-    [SerializeField] float idlingDistance = 2f;
+    [SerializeField] float idlingDistanceFollowing = 2f;
 
     // Display remain distance in Inspector
     [SerializeField] float dispRemainDistance = 0f;
 
-    Animator anim;
+    public static Transform targetPoint;
 
-    // Position
-    Vector3 pos;
+    private NavMeshAgent agent;
 
+    float minimumWalk;
+
+    private Animator anim;
+
+    private Vector3 positionVector;
+
+    /// <summary>
+    /// Initialize all the variables with objects upon game starts.
+    /// </summary>
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
 
         anim = GetComponent<Animator>();
+
+        GameObject targetObject = GameObject.Find("Target");
+
+        targetPoint = targetObject.transform;
+
+        agent.radius = 0.1f;
 
         // No brake when near obstacle
         agent.autoBraking = false;
@@ -43,10 +50,13 @@ public class EnemyMovement : MonoBehaviour
         // No rotation while walking
         agent.updateRotation = false;
 
-        NextTarget();
+        MoveToNextTarget();
     }
 
-    // Enemy follows player called by TriggerEvent 
+    /// <summary>
+    /// Enemy starts following player once player touches trigger collider
+    /// </summary>
+    /// <param name="collider">object that touched this collider</param>
     public void FollowPlayer(Collider collider)
     {
         Debug.Log("Start following");
@@ -54,13 +64,18 @@ public class EnemyMovement : MonoBehaviour
 
         if (collider.name.Equals("Player")) {
 
+            // Rotates enemy before changing direction
+            Quaternion rotation = GetQuaternion(collider.transform.position - transform.position, Vector3.zero);
+
+            transform.rotation = rotation;
+
             // Set player's position as a next target
             agent.destination = collider.transform.position;
 
             // Displaying remain distance in Inspector
             dispRemainDistance = agent.remainingDistance;
 
-            if (agent.remainingDistance < idlingDistance)
+            if (agent.remainingDistance < idlingDistanceFollowing)
             {
                 IdlePoint();
             }
@@ -70,33 +85,67 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-    // Generate the next position and move
-    void NextTarget()
+    /// <summary>
+    /// Generate the next position and move
+    /// </summary>
+    void MoveToNextTarget()
     {
+        float position_X;
+        float position_Z;
+        int randSign_X;
+        int randSign_Z;
+
         agent.isStopped = false;
 
+        minimumWalk = randArea / 2;
+
+        positionVector = targetPoint.position;
+
         // Generate the next target point
-        float position_X = Random.Range(-1 * randArea, randArea);
-        float position_Z = Random.Range(-1 * randArea, randArea);
+        position_X = Random.Range(minimumWalk, randArea);
+        position_Z = Random.Range(minimumWalk, randArea);
+        randSign_X = Random.Range(0, 5) % 2;
+        randSign_Z = Random.Range(0, 5) % 2;
 
-        pos = targetPoint.position;
-        pos.x += position_X;
-        pos.z += position_Z;
+        if (randSign_X == 0)
+        {
+            position_X = position_X * -1;
+        }
 
-        // Set the direction based on the next point
-        Vector3 direction = new Vector3(pos.x, transform.position.y, pos.z);
+        if (randSign_Z == 0)
+        {
+            position_Z = position_Z * -1;
+        }
+
+        positionVector.x += position_X;
+        positionVector.z += position_Z;
 
         // Rotates enemy before start walking to the next point
-        Quaternion rotation = Quaternion.LookRotation(direction - transform.position, Vector3.up);
+        Quaternion rotation = GetQuaternion(positionVector - transform.position, Vector3.zero);
+
         transform.rotation = rotation;
 
-        agent.destination = pos;
+        agent.destination = positionVector;
     }
 
+    /// <summary>
+    /// Get quaternion.
+    /// </summary>
+    /// <param name="distanceVector3"></param>
+    /// <param name="vector3">vector3 for center</param>
+    /// <returns>Quaternion</returns>
+    Quaternion GetQuaternion(Vector3 distanceVector3, Vector3 vector3) {
+        return Quaternion.LookRotation(distanceVector3, vector3);
+    }
+
+    /// <summary>
+    /// Enemy idle behavior.
+    /// </summary>
     void Update()
     {
         // Enemy idle while waiting for the next point and the remaining distance is less than 0.5
-        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+        if(!agent.pathPending && agent.remainingDistance < minimumWalk) { 
+        //if (agent.remainingDistance == 0) {
             IdlePoint();
         }
 
@@ -104,7 +153,9 @@ public class EnemyMovement : MonoBehaviour
         anim.SetFloat("Blend", agent.velocity.sqrMagnitude);
     }
 
-    // Behavior at the destination. Count time then generate next point.
+    /// <summary>
+    /// Idle behavior.
+    /// </summary>
     void IdlePoint()
     {
         agent.isStopped = true;
@@ -112,7 +163,7 @@ public class EnemyMovement : MonoBehaviour
 
         if (countWaitingTime > waitingTime)
         {
-            NextTarget();
+            MoveToNextTarget();
             countWaitingTime = 0;
         }
     }
--- Assets/Entities/EnemyTrigger.cs
+++ Assets/Entities/EnemyTrigger.cs
@@ -2,19 +2,51 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(SphereCollider))]
+
+/// <summary>
+/// Handle trigger. Enemy follows player while player is in a certain distance 
+/// </summary>
+
 public class EnemyTrigger : MonoBehaviour
 {
-    // Set an event to trigger following the player.
-    [SerializeField] private TriggerEvent stay = new TriggerEvent();
 
-    // Once it's triggered, it keeps triggering while colliding.
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Player is inside!");
-        stay.Invoke(other);
+    [SerializeField] private TriggerEvent _stay = new TriggerEvent();
+
+    private SphereCollider sphereCollider;
+
+    /// <summary>
+    /// Initialize radius centered enemy.
+    /// Enemy will follow once player touches collider.
+    /// </summary>
+    void Start() {
+        sphereCollider = GetComponent<SphereCollider>();
+        sphereCollider.radius = 1f;
     }
 
-    // An event handler with receiveing collider object
+    /// <summary>
+    /// Event function that triggers by colliding objects.
+    /// If passed collider is tagged as "Player", then enemy will start follow.
+    /// </summary>
+    /// <param name="other"> is an object that touched this collider</param>
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.gameObject + " is inside");
+        if (other.CompareTag("Player")) {
+            _stay.Invoke(other);
+        }
+    }
+
+    /// <summary>
+    /// Collision event will be called once a specified object touches the collider.
+    /// </summary>
+    /// <remarks>
+    /// To set up this collision event behavior on Inspectot:
+    ///     Add a relation in 'Stay(Collider)'
+    ///     Drag and add Enemy object
+    ///     Once set an object, Function should be able to select on the right.
+    ///     Scroll to 'EnemyMovement.cs' and choose 'FollowPlayer' function.
+    /// </remarks>
     [Serializable]
     public class TriggerEvent : UnityEvent<Collider>
     {
