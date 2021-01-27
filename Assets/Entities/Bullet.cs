9581dd50ea5e782749093d7946bed5026387815e
Canyon-Artuz <canyonartuz@gmail.com>
2021-01-26 22:28:57 -0800

    Super Rough Working Model
    Gimmicky but it works...  I'll finish it off tomorrow.
    Basically, it shoots in the right direction but only sometimes.  I have no idea why, probably something to do with my bullet prefab.

--- Assets/Entities/Bullet.cs
+++ Assets/Entities/Bullet.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
-
-    private void OnCollisionEnter(Collision collision) {
-        // Instantiate the Hit Effect at the given position and roation of it's parent.
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        // Destroy the bullet.
+    void OnCollisionEnter(Collision collision)
+    {
         Destroy(gameObject);
-        // Destroy the effect after 5 seconds.
-        Destroy(effect, 5f);
+    }
+
+    void Start()
+    {
+        Destroy(gameObject, 5f);
     }
 }
---
+++ Assets/Entities/PlayerControls.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+    // Speed variable.  Adjustable in Editor thanks to SerializeField.
+    [SerializeField] float movementSpeed;
+    // Ensures we only aim at the ground and not, like, walls.
+    [SerializeField] LayerMask aimLayer;
+    // The animator component.
+    Animator animator;
+    // Where the bullet comes out.
+    public Transform ejector;
+    // Creates a field in which you can decide how long the delay is between shots.
+    [SerializeField] float shotDelay;
+    // Float that determines when the Player is able to shoot again.
+    float timeToShoot;
+    // What bullet gets fired.
+    [SerializeField] Bullet bulletPrefab;
+
+    [SerializeField] float bulletSpeed = 15f;
+
+    // Starts up the entire animation process.  As this will be changed across the entire game, Awake is necessary instead of Start.
+    private void Awake() => animator = GetComponent<Animator>();
+
+    void Update() {
+        Move();
+        // Should be mapped to Left Click for now.
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Move()
+    {
+        // Tells the Animator you're not firing, you're moving.  Cancels out the Firing animation.  Can be improved later.
+        animator.SetBool("Firing", false);
+        // Create a Ray from the Main Character to the Mouse's position.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // Use Physics to create a Raycast out of the ground, the max distance to shoot, infinity, and the ground layer.
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayer))
+        {
+            // Calculates direction.
+            var direction = hitInfo.point - transform.position;
+            // Consider the forward to be the direction of the mouse.
+            transform.forward = direction;
+        }
+
+        // Reads input.
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Makes sure that A and D go on the Horizontal and W and S go on the Vertical
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+
+        // Moving
+        if (movement.magnitude > 0)
+        {
+            // Adjust the speed according to framerate.
+            movement *= movementSpeed * Time.deltaTime;
+            // Moves object according to worldspace.
+            transform.Translate(movement, Space.World);
+        }
+
+        // Animating using Dot Product of normalized movement and the forward transform.
+        float velocityZ = Vector3.Dot(movement.normalized, transform.forward);
+        // Animating using Dot Product of normalized movement and the right transform.
+        float velocityX = Vector3.Dot(movement.normalized, transform.right);
+        // Changes the position of the Player Model and smoothly transitions between animations.
+        animator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
+        animator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
+    }
+
+    // Determines if the gun is able to shoot yet.
+    bool shotReady() => Time.time >= timeToShoot;
+
+    void Shoot()
+    {
+        // Sets the next time you're able to shoot.
+        timeToShoot = Time.time + shotDelay;
+        /*
+        // Instantiate a Bullet prefab.
+        Bullet bullet = Instantiate(bulletPrefab, ejector.position, transform.rotation);
+        // Tells the bullet to shoot "forward"
+        bullet.Launch(transform.forward * 1f);
+        */
+        
+        Bullet bullet = Instantiate(bulletPrefab, ejector.position, Quaternion.identity);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * bulletSpeed;
+        
+        // Tell the animator to play the "Firing" animation.
+        animator.SetBool("Firing", true);
+    }
+
+    void AimTowardMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, Mathf.Infinity, aimLayer))
+        {
+            var destination = hitInfo.point;
+            destination.y = transform.position.y;
+
+            Vector3 direction = destination - transform.position;
+            direction.Normalize();
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+        }
+    }
+}
