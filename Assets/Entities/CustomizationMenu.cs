958304f75dbd46957e780084d0071fc8eff61904
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2021-02-06 16:46:18 -0800

    Adder Player to Canvas
    Made background transparent inorder to view player.

---
+++ Assets/Entities/CustomizationMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 
+/// </summary>
+public class CustomizationMenu : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public void NextItem(GameObject button)
+    {
+        Debug.Log("button pressed: " + button.name);
+        if (button.name == "LeftButton")
+        {
+            Debug.Log("left button pressed");
+            /// 
+        }
+        else if (button.name == "RightButton")
+        {
+            Debug.Log("right button pressed");
+        }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Save()
+    {
+        Debug.Log("Saving...");
+
+    }
+
+}
