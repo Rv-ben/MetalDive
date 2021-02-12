364365f04858a45271f3d8c3cf6ddcfe6f1e7c91
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2021-02-12 13:59:13 -0800

    Current Customization's NOT WORKING
    putting this on hold...

--- Assets/Entities/CustomizationMenu.cs
+++ Assets/Entities/CustomizationMenu.cs
@@ -8,21 +8,45 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class CustomizationMenu : MonoBehaviour
 {
+
+    private Player player;
+    
+    // available hairstyles
+    private List<GameObject> hairstyles = new List<GameObject>();
+
+    // 
+    private int currentOption = 0;
+
+    public void UpdateCustomization(GameObject options)         // eventually have 2 params (player's body part, option)
+    {   
+        // update prefab here
+    }
+
+
     /// <summary>
     /// 
     /// </summary>
     public void NextItem(GameObject button)
     {
-        Debug.Log("button pressed: " + button.name);
         if (button.name == "LeftButton")
         {
+            if (currentOption == 0) // in 1st item in list/index 0, can't go left, start from end index/size-1
+            {
+                currentOption = hairstyles.Count - 1;
+            }
+            // update current index to previous
+            currentOption--;
             Debug.Log("left button pressed");
             /// 
         }
         else if (button.name == "RightButton")
         {
+            // update current index to next
+            currentOption++;
             Debug.Log("right button pressed");
         }
+
+        UpdateCustomization(hairstyles[currentOption]);
     }
 
 
