4460cb0a0328390ba4f909e2e29f2c16204639b5
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2021-02-23 16:55:13 -0800

    Added canvas for loading screen with slider for Play-Game
    also updated unity version
    not 100% done, need to slow down screen to show progression of slider value

--- Assets/Entities/MainMenu.cs
+++ Assets/Entities/MainMenu.cs
@@ -1,32 +1,105 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;  // LoadScene()
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    private GameObject CharacterCanvas;
-    /**
-     * Method called from on click event 
-     */
+    // specified time from Unity
+    [SerializeField] private float waitTime;
+
+    public GameObject loadingCanvas;
+
+    public Slider progressBar;
+
+    public TMP_Text progressText;
+
+    /// <summary>
+    /// Method called from on click event, waits for specified time
+    /// to show loading canvas, then loads new scene
+    /// </summary>
     public void PlayGame()
     {
-        //CharacterCanvas = GetComponent(Canvas);
-        // Scene manager loads the following scene in queue
-        // Queue for scenes is in Unity's Build Settings
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        // show loading canvas
+        //loadingCanvas.SetActive(true);
 
+        //int next = SceneManager.GetActiveScene().buildIndex + 1;
+
+        // start coroutine to wait for specified time
+        StartCoroutine(LoadCanvas(waitTime));
+
+        
+        // Scene manager loads the following scene in queue (from Unity build settings)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   // ******************WORKS
     }
 
-    /**
-     * Method called from on click event 
-     */
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    IEnumerator LoadCanvas(float waitTime)
+    {
+        float progress = 0;
+        float count = 0;
+
+        for (float i = 0; i < waitTime; ++i)
+        {
+            // calculate progress 
+            progress = waitTime / (Mathf.Pow(waitTime, 2) / (i + 1));
+
+            // update slider values with new progress
+            this.UpdateSlider(progress);
+
+
+            // if progress at MAX possible value for slider, break
+            if (progress == 1)
+            {
+                Debug.Log("progress is 1");
+                count = progress;
+                break;
+            }
+
+            //yield return new WaitForSeconds(waitTime);
+
+
+            //yield return null;
+            //yield return new WaitForSeconds(time);
+        }
+        loadingCanvas.SetActive(true);
+        yield return new WaitForSeconds(waitTime); // **************************WORKS
+        //yield return new WaitUntil(() => count == 1);
+    }
+
+
+    /// <summary>   
+    /// Method called from on click event 
+    /// </summary>
     public void QuitGame()
     {   
         // Debug comment in console
         Debug.Log("Quitting game...");
+
         // Shuts down application
         Application.Quit();
+    }
+
+
+    /// <summary>
+    /// update progress bar value and text
+    /// </summary>
+    public void UpdateSlider(float progress)
+    {
+
+        Debug.Log("updating slide with progress: " + progress);
+
+        // update slider value to new progress value
+        progressBar.value = progress;
+        // update slider text to new progress value as a percentage
+        progressText.text = progress * 100f + "%";
+        
+        Debug.Log("progressBar value:text: " + progressBar.value + ":" + progressText.text);
     }
 
 }
