fb921dcad4080c00b6ddcd8508a610feeb7a1c84
aimilbc <55858907+aimilbc@users.noreply.github.com>
2021-03-09 18:26:49 -0800

    script deleted

--- Assets/Scripts/ObjectSpawning/HealthBox.cs
+++ Assets/Scripts/ObjectSpawning/HealthBox.cs
@@ -2,12 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthBox : ObjectSpawning
-{
 
-    public override void OnTriggerEnter(Collider other)
-    {
-        // If other is player, then player can pickup health.
-    }
-
-}
