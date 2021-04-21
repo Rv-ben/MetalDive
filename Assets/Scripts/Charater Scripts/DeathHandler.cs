e103105c2476cee9fee45428d6048af99904da6e
Canyon-Artuz <canyonartuz@gmail.com>
2021-04-20 19:12:03 -0700

    Animation Plays but it doesn't stay dead

--- Assets/Scripts/Charater Scripts/DeathHandler.cs
+++ Assets/Scripts/Charater Scripts/DeathHandler.cs
@@ -7,6 +7,7 @@ public class DeathHandler : MonoBehaviour
     public HealthSlider healthSlider;
     public Animator animator;
     public PlayerMovement player;
+    public EnemyMovement enemy;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +15,16 @@ public class DeathHandler : MonoBehaviour
         // Gets all the components we need.
         healthSlider = GetComponent<HealthSlider>();
         animator = GetComponent<Animator>();
-        player = GetComponent<PlayerMovement>();
+        if (gameObject.tag == "Player")
+        {
+            player = GetComponent<PlayerMovement>();
+            enemy = null;
+        }
+        else if (gameObject.tag == "Enemy")
+        {
+            enemy.GetComponent<EnemyMovement>();
+            player = null;
+        }
     }
 
     // Update is called once per frame
@@ -23,14 +33,23 @@ public class DeathHandler : MonoBehaviour
         // Whenever the Player loses all their health the first time.
         if (healthSlider.healthValue == 0 && !animator.GetBool("TotallyDead"))
         {
+            animator.StopPlayback();
+            Debug.Log("FUCKING DIE DAMN YOU");
             // Triggers the death animation.
             animator.SetTrigger("Dying");
+            if (gameObject.tag == "Player")
+            {
+                // This should stop them in their tracks, like a good corpse.
+                player.movementSpeed = 0;
+                // With luck this'll prevent the Player from looking around.
+                player.aimLayer = default;
+            }
+            else if (gameObject.tag == "Enemy")
+            {
+                enemy.walkingSpeed = 0;
+            }
             // Prevents them from just getting back up.
             animator.SetBool("TotallyDead", true);
-            // This should stop them in their tracks, like a good corpse.
-            player.movementSpeed = 0;
-            // With luck this'll prevent the Player from looking around.
-            player.aimLayer = default;
         }
     }
 }
