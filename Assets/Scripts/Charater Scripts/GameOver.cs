2234b22674b4cf9bb7a581e3ad472d65083a37ac
Canyon-Artuz <canyonartuz@gmail.com>
2021-04-29 19:22:28 -0700

    Update GameOver.cs

--- Assets/Scripts/Charater Scripts/GameOver.cs
+++ Assets/Scripts/Charater Scripts/GameOver.cs
@@ -7,20 +7,21 @@ public class GameOver : MonoBehaviour
 {
     public ElevatorSwitch switcher;
     public DeathHandler death;
+    public bool end;
 
     // Start is called before the first frame update
     void Start()
     {
         switcher = GameObject.FindGameObjectWithTag("Elevator").GetComponent<ElevatorSwitch>();
         death = GameObject.FindGameObjectWithTag("Player").GetComponent<DeathHandler>();
+        end = false;
     }
 
     void Update()
     {
-        if (switcher.playerEnter || death.gameOver)
+        if (switcher.playerEnter || death.gameOver )
         {
-            // Needs to hardcode the game over scene.  Currently, this is not the case.
-            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            end = true;
         }
     }
 }
