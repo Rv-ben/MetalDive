b150d4ce671ca5f4121b23a8ea8af11715663674
aimilbc <55858907+aimilbc@users.noreply.github.com>
2021-04-29 21:10:46 -0700

    Added destroyEnvironment function

---
+++ Assets/Scripts/Level Generation/ProceedNextLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceedNextLevel : MonoBehaviour
+{
+    private List<string> FloorObjects;
+    /// <summary>
+    /// Initialize all the variables with objects upon game starts.
+    /// </summary>
+    void Start()
+    {
+        FloorObjects = new List<string>(){"Barrier(Clone)", "Bitcoin(Clone)", "Healthkit(Clone)", "Elevator(Clone)", "Wall", "Floor"};
+    }
+
+    /// <summary>
+    /// Destroys all the object that have the same name as saved in FloorObjects.
+    /// </summary>
+    public void destroyEnvironment()
+    {
+        foreach (GameObject obj in Object.FindObjectsOfType(typeof(GameObject)))
+        {
+            if (FloorObjects.Contains(obj.name))
+            {
+                Destroy(obj);
+            }
+        }
+    }
+}
