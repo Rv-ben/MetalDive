c3e40a8a15a7eeaceb648f7c173515aae9bd0ba8
Rv-ben <Ruben.Bramasco@Student.Csulb.edu>
2021-05-06 18:10:02 -0700

    updates various small details

--- Assets/Scripts/Charater Scripts/Shooting.cs
+++ Assets/Scripts/Charater Scripts/Shooting.cs
@@ -22,6 +22,9 @@ public class Shooting : MonoBehaviour
     // Dictionary tying an Enum to a String, which is used to determine an animation to play.
     public Dictionary<WeaponEnum, string> animationDictionary;
 
+    public float shotDelay = 0f;
+    public float fireTime = 0f;
+
     /// <summary>
     /// Checks if the Player is able to shoot their weapon yet.
     /// </summary>
@@ -38,6 +41,9 @@ public class Shooting : MonoBehaviour
         animationDictionary.Add(WeaponEnum.SciFiHandGun, "FiringPistol");
         // Ties the Guitar Playing Animation to the Guitar state.
         animationDictionary.Add(WeaponEnum.ElectricGuitar, "PlayingGuitar");
+        shotDelay = bullet.GetComponent<Bullet>().getShotDelay();
+        fireTime = 0;
+
 
         // BELOW IS TEMPORARY.
         // Player is not armed.
@@ -53,11 +59,29 @@ public class Shooting : MonoBehaviour
     /// </summary>
     public void Update()
     {
-        // Checks if LClick is pushed.
-        if (Input.GetMouseButtonDown(0) && !GameObject.Find("GameManager").GetComponent<GameManager>().isPaused)
+        if (!GameObject.Find("GameManager").GetComponent<GameManager>().isPaused && Time.time > fireTime )
         {
-            // Shooting logic.  Passes in whatever state it's at, as well as the bullet to be fired.
-            Shoot(animEnum, bullet);
+            if(this.gameObject.tag == "Player")
+            {
+                // Checks if LClick is pushed.
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log(this.gameObject);
+                    fireTime = Time.time + shotDelay;
+                    // Shooting logic.  Passes in whatever state it's at, as well as the bullet to be fired.
+                    Shoot(animEnum, bullet);
+                }
+            }
+            else
+            {
+                var trigger = this.gameObject.transform.GetChild(4).gameObject;
+                Debug.Log(trigger);
+                if (trigger.GetComponent<TriggerEnemy>().isFollowing)
+                {
+                    fireTime = Time.time + shotDelay;
+                    Shoot(animEnum, bullet);
+                }
+            }
         }
     }
 
@@ -77,13 +101,18 @@ public class Shooting : MonoBehaviour
         {
             // Instantiates a pre-chosen bullet at specified ejector facing the same way as the ejector.
             GameObject bull = Instantiate(bullet, ejector.position, ejector.rotation);
-            bull.GetComponent<Rigidbody>().velocity = 
-                // The forward transform - Makes sure it always faces the forward position of the ejector.s
-                transform.forward + 
-                // Create a new vector 3 Direction skewed randomly n the Z axis by the + and - spread of the bullet, but otherwise unchanging in it's X or Y planes.
-                new Vector3(0, 0, Random.Range(-bullet.GetComponent<Bullet>().getSpread(), bullet.GetComponent<Bullet>().getSpread())) * 
-                // Launch the bullet forward with it's inherent speed value.
-                bullet.GetComponent<Bullet>().getBulletSpeed();
+
+            var direction =
+                Random.Range(-bullet.GetComponent<Bullet>().getSpread(), bullet.GetComponent<Bullet>().getSpread());
+
+            var bulletSpeed = bullet.GetComponent<Bullet>().getBulletSpeed();
+
+            var speed = new Vector3(bulletSpeed, bulletSpeed, bulletSpeed);
+
+            bull.GetComponent<Rigidbody>().velocity = Vector3.Scale(transform.forward, speed);
+            // The forward transform - Makes sure it always faces the forward position of the ejector.s
+
+            //bull.GetComponent<Rigidbody>().velocity.magnitude *= bullet.GetComponent<Bullet>().getBulletSpeed();
         }
         // Reset the time to shoot.
         timeToShoot = 0;
--- Assets/Scripts/Charater Scripts/TriggerEnemy.cs
+++ Assets/Scripts/Charater Scripts/TriggerEnemy.cs
@@ -8,6 +8,13 @@ public class TriggerEnemy : MonoBehaviour
 
     [SerializeField] private TriggerEvent _stay = new TriggerEvent();
 
+    public bool isFollowing;
+
+    void Start()
+    {
+        isFollowing = false;
+    }
+
     /// <summary>
     /// Event function that triggers by colliding objects.
     /// If passed collider is tagged as "Player", then enemy will start follow.
@@ -20,4 +27,23 @@ public class TriggerEnemy : MonoBehaviour
             _stay.Invoke(other);
         }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFollowing = true;
+            _stay.Invoke(other);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isFollowing = false;
+            _stay.Invoke(other);
+        }
+    }
+
 }
